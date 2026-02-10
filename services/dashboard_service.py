0318ce5a80baec116a5edb7b9d19a10de09a6e8c
Mohamed DIOP <diopmedzo91@gmail.com>
2026-02-10 10:02:38 +0100

    refactor: update historical repository and dashboard service to use new historical totals structure

--- services/dashboard_service.py
+++ services/dashboard_service.py
@@ -1,16 +1,19 @@
 from database.trip_repository import TripRepository
 from database.fuel_repository import FuelRepository
+from database.historical_repository import HistoricalRepository
 from utils.calculations import Calculations
 
 class DashboardService:
     def __init__(self):
         self.repo = TripRepository()
         self.fuel_repo = FuelRepository()
+        self.history_repo = HistoricalRepository()
 
     def get_kpis(self):
         trips = self.repo.get_trips()
         fuels = self.fuel_repo.get_fuels()
-        total_ca = Calculations.total_CA(trips)
-        total_fuels = Calculations.total_fuels(fuels)
+        history = self.history_repo.get_totals()
+        total_ca = Calculations.total_CA(trips) + history.historique_CA
+        total_fuels = Calculations.total_fuels(fuels) + history.historique_carburants
 
         return total_ca, total_fuels, total_ca - total_fuels
