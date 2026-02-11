5bae590ffd3ac65f6b657ca504d30ff5c8041ed9
Mohamed DIOP <diopmedzo91@gmail.com>
2026-02-11 09:51:11 +0100

    changing the structure of DashboardKPIs by doing beatiful colors (background_color)

--- ui/tabs/dashboard.py
+++ ui/tabs/dashboard.py
@@ -7,10 +7,10 @@ def render_dashboard():
     kpis = DashboardService().get_kpis()
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-        KPICard(label="Chiffre d'affaires", value=kpis.total_ca, icon="💰", background_color="#454748", text_color="#E6F8EDDF").render()
+        KPICard(label="Chiffre d'affaires", value=kpis.total_ca, icon="💰", background_color="#0E1117", text_color="#E6EDF3").render()
     with col2:
-        KPICard(label="Dépenses carburant", value=kpis.total_carburant, icon="⛽", background_color="#2196F3", text_color="#F6071B").render()
+        KPICard(label="Dépenses carburant", value=kpis.total_carburant, icon="⛽", background_color="#0E1117", text_color="#E6EDF3").render()
     with col3:
-        KPICard(label="Bénéfices", value=kpis.benefice_total, icon="📈", background_color="#2196F3", text_color="#29EB07").render()
+        KPICard(label="Bénéfices", value=kpis.benefice_total, icon="📈", background_color="#0E1117", text_color="#E6EDF3").render()
     with col4:
-        KPICard(label="Dernier plein", value=kpis.dernier_plein, icon="⛽", background_color="#2196F3", text_color="#E6F8EDDF").render()
+        KPICard(label="Dernier plein", value=kpis.dernier_plein, icon="⛽", background_color="#0E1117", text_color="#E6EDF3").render()
